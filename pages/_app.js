cf9f3f1cddd04fac54adcb9a5645a72e89588b3f
Noyan <noyan.alimov@gmail.com>
2022-09-25 00:34:57 +0100

    solana test

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,10 @@
 import '../styles/globals.css'
+import { Wallet } from '../components/Wallet'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Wallet>
+    <Component {...pageProps} />
+  </Wallet>
 }
 
 export default MyApp
